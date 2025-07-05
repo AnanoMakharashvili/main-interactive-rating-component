052e46b453d3cef973597c6a199adcfafa3dd650
AnanoMakharashvili <ananomakharashvili@gmail.com>
2025-07-05 17:23:46 +0300

    fix: small JSX fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
       style={{
         backgroundColor: "#FC7614",
         color: "#FFFFFF",
-        padding: "12px 109px",
+        padding: "12px 110px",
         borderRadius: "22.5px",
         border: "none",
       }}
